2c170aba5771fa1d72a996a04a171c27f7ea0250
rlagaronh-commits <rlagaron.h@gmail.com>
2026-04-24 17:51:02 +0200

    2.0.4

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'gasofriends-v2.0.4';
+const APP_SHELL = ['./', './index.html', './offline.html', './manifest.webmanifest'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME && key.startsWith('gasofriends-')).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./offline.html'))));
+});
